f5ab11f5f47d3d43c8ea0dfd0ab65aaff8b6c28d
Chris Underwood <cunderwoodmn@gmail.com>
2019-12-14 22:28:14 -0500

    implement default properties for dragon. getter for date to be dynamic and real time

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,7 @@ const Dragon = require('./dragon')
 
 const fooey = new Dragon({birthdate: new Date(), nickname: 'fooey'});
 const baloo = new Dragon({birthdate: new Date(), nickname: 'baloo'});
+const boop = new Dragon({});
 console.log("fooey", fooey);
 console.log("baloo", baloo);
+console.log("boop", boop);
