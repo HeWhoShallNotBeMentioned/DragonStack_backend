5e1c4b29826e9a50dfa56c7603a22ea729bc7ec9
Chris Underwood <cunderwoodmn@gmail.com>
2019-12-15 13:51:41 -0500

    add nodemon to backend and add delayed creation dragon

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,8 @@ const boop = new Dragon({});
 console.log("fooey", fooey);
 console.log("baloo", baloo);
 console.log("boop", boop);
+
+setTimeout(()=> {
+  const oopty = new Dragon({nickname: "oopty"})
+  console.log("oopty",oopty)
+}, 2500)
