a08d38bc489acb02ae8009648183e64a00b1baa6
Chris Underwood <cunderwoodmn@gmail.com>
2019-12-25 19:32:40 -0500

    updated jshint for strict and tested the database connection

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -4,3 +4,11 @@ const databaseConfiguration = require('./secrets/databaseConfiguration');
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
+
+pool.query('SELECT * FROM generation', (error, response) => {
+  if (error) {
+    console.error('error', error);
+  } else {
+    console.log('response.rows', response.rows);
+  }
+});
