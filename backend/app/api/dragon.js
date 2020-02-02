3bf394a82d280086835ba6eaa0e1849c4daea81e
Chris Underwood <cunderwoodmn@gmail.com>
2020-02-01 22:51:54 -0500

    add abililty to save a dragon to the database

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -1,9 +1,19 @@
 const { Router } = require('express');
+const DragonTable = require('../dragon/table');
 
 const router = new Router();
 
-router.get('/new', (req, res) => {
-  res.json({ dragon: req.app.locals.engine.generation.newDragon() });
+router.get('/new', async (req, res) => {
+  try {
+    const dragon = req.app.locals.engine.generation.newDragon();
+    const dragonId = await DragonTable.storeDragon(dragon);
+    console.log('dragonId--- ', dragonId);
+    dragon.dragonId = dragonId;
+
+    res.json({ dragon });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
