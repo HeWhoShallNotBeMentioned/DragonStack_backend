ef635bf16c8bf1af09c09f1f9dc115913b69e7cd
Chris Underwood <cunderwoodmn@gmail.com>
2020-02-03 21:26:20 -0500

    add traits table that pulls in data from a JSON file

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Configuring dragonstackdb"
 
 dropdb -U node_user dragonstackdb
@@ -7,5 +8,8 @@ createdb -U node_user dragonstackdb
 
 psql -U node_user dragonstackdb < ./bin/sql/generation.sql
 psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
+psql -U node_user dragonstackdb < ./bin/sql/trait.sql
+
+node ./bin/insertTraits.js
 
 echo "Finished configuring dragonstackdb"
