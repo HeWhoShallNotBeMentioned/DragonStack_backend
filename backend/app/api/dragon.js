cbafbdc120185465690d455c92fc633c78683f2d
Chris Underwood <cunderwoodmn@gmail.com>
2020-02-06 12:21:27 -0500

    add route for one dragon

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const DragonTable = require('../dragon/table');
+const { getDragonWithTraits } = require('../dragon/helper');
 
 const router = new Router();
 
@@ -16,5 +17,20 @@ router.get('/new', async (req, res, next) => {
     next(error);
   }
 });
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    console.log("id", req.params.id);
+    console.log("Inside get dragon by id route");
+    const dragon = await getDragonWithTraits({ dragonId: req.params.id });
+    res.json(dragon);
+  } catch (error) {
+    console.error(error);
+    next(error)
+  }
+
+
+
+})
 
 module.exports = router;
