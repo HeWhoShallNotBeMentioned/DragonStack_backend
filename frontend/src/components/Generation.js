21c126953bbc670050a7bfa40a6a12acedba1379
Chris Underwood <cunderwoodmn@gmail.com>
2020-02-11 11:30:11 -0500

    add fetch to grab current generation. Failed due to CORS policy

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import regeneratorRuntime from "regenerator-runtime";
 
 class Generation extends Component {
+  state = {
+    generation: { generationId: 99999, expiration: "2020-05-01" },
+  };
+
+  async componentDidMount() {
+    this.fetchGeneration();
+  };
+
+  async fetchGeneration() {
+    const data = await fetch('http://localhost:3000/generation');
+    console.log("generation data", data)
+  };
 
   render() {
 
-    const generation = { generationId: 99999, expiration: "2020-05-01" };
+
     return (<div>
-      <h3>Generation {generation.generationId} Expires on: </h3>
-      <h4>{new Date(generation.expiration).toString()}</h4>
+
+      <h3>Generation {this.state.generation.generationId} Expires on: </h3>
+      <h4>{new Date(this.state.generation.expiration).toString()}</h4>
+
     </div>)
   }
-
 }
 
 export default Generation;
