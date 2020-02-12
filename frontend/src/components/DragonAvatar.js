edcd729206cee75b7b0f8b62aece7c6ff5b553f7
Chris Underwood <cunderwoodmn@gmail.com>
2020-02-12 13:13:17 -0500

    add propertyMap to DragonAvatar and dragon images to assets

--- frontend/src/components/DragonAvatar.js
+++ frontend/src/components/DragonAvatar.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
+import { patchy, plain, skinny, slender, spotted, stocky, striped, sporty } from '../assets/index'
 import regeneratorRuntime from "regenerator-runtime";
+
+const propertyMap = {
+  backgroundColor: {
+    black: '#263238',
+    white: '#cfd8dc',
+    green: '#a5d6a7',
+    blue: '#0277bd'
+  },
+  build: { slender, stocky, sporty, skinny, },
+  pattern: { plain, striped, spotted, patchy },
+  size: { small: 100, medium: 140, large: 180, enormous: 220 }
+}
 
 class DragonAvatar extends Component {
 
@@ -13,7 +26,8 @@ class DragonAvatar extends Component {
         <span>Dragon Id:  {this.props.dragon.dragonId}.</span>
         {this.props.dragon.traits.map((trait, id) => {
           return (
-            <span key={id}>{trait.traitType}-{trait.traitValue}, </span>)
+            <span key={id}>{trait.traitType === "backgroundColor"
+              ? "background color" : trait.traitType}: {trait.traitValue}, </span>)
         })}
 
       </div>
