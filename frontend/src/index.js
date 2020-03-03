677ba8f3d4d6afc7f9bf3eadd49fbae041f11a8d
Chris Underwood <cunderwoodmn@gmail.com>
2020-03-02 21:45:31 -0500

    add and action to the reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,25 +4,38 @@ import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 
-
 import './index.css';
 
 const DEFAULT_GENERATION = { generationId: '', expiration: '' };
 
-const generationReducer = () => {
-  return { generation: DEFAULT_GENERATION }
+const generationReducer = (state, action) => {
+  console.log('generation state', state);
+  console.log('generation action', action);
+
+  if (action.type === 'GENERATION_ACTION_TYPE') {
+    return { generation: action.generation };
+  }
+
+  return { generation: DEFAULT_GENERATION };
 };
 
 const store = createStore(generationReducer);
-console.log("store ", store);
-console.log("store.getState()", store.getState());
+console.log('store ', store);
+console.log('store.getState()', store.getState());
+
+store.dispatch({ type: 'foo' });
+store.dispatch({
+  type: 'GENERATION_ACTION_TYPE',
+  generation: { generationId: 'goo', expiration: 'bar' },
+});
+
+console.log('store.getState()', store.getState());
 
 render(
   <div>
     <h2>Dragon Stack</h2>
     <Generation />
     <Dragon />
-  </div>
-  ,
+  </div>,
   document.getElementById('root')
 );
