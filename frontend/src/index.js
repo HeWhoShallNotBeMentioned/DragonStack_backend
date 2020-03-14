0820795705dc086770caddabd53e0fbdc258b41d
Chris Underwood <cunderwoodmn@gmail.com>
2020-03-14 19:06:32 -0400

    organized redux code for generation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,50 +4,21 @@ import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 
+import { generationReducer } from './reducers/index.js'
+import { generationActionCreator } from './actions/generation'
 import './index.css';
 
-const DEFAULT_GENERATION = { generationId: '', expiration: '' };
 
-const GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE';
 
-const generationReducer = (state, action) => {
-
-  if (action.type === GENERATION_ACTION_TYPE) {
-    return { generation: action.generation };
-  }
-
-  return { generation: DEFAULT_GENERATION };
-};
-
-const store = createStore(generationReducer);
+const store = createStore(generationReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 store.subscribe(() => { return console.log('store state change', store.getState()) });
-
-console.log('store ', store);
-console.log('store.getState()', store.getState());
-
-store.dispatch({ type: 'foo' });
-store.dispatch({
-  type: GENERATION_ACTION_TYPE,
-  generation: { generationId: 'goo', expiration: 'bar' },
-});
-
-console.log('store.getState()', store.getState());
-
-const generationActionCreator = (payload) => {
-  return {
-    type: GENERATION_ACTION_TYPE,
-    generation: payload,
-  }
-}
 
 fetch('http://localhost:3000/generation')
   .then(response => response.json())
   .then(json => {
     store.dispatch(generationActionCreator(json.generation))
   })
-
-
 
 render(
   <div>
