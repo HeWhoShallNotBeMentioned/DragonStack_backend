8a379f06f8d5e1c9f946831d0cf1ee53b47609bd
Chris Underwood <cunderwoodmn@gmail.com>
2020-03-23 22:15:58 -0400

    add table.js which will save accounts to the database

---
+++ backend/app/account/table.js
@@ -0,0 +1,23 @@
+const pool = require('../../databasePool');
+
+class AccountTable {
+  static storeAccount({ username, password }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'INSERT INTO account(usernmae, password) VALUES($1, $2) RETURNING id',
+        [username, password],
+        (error, response) => {
+          if (error) {
+            return reject(error);
+          }
+
+          const accountId = response.rows[0].id;
+
+          resolve({ accountId: accountId });
+        }
+      )
+    })
+  }
+}
+
+module.exports = AccountTable;
