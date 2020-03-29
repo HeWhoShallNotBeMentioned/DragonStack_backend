399ac193c8a8777e63dea0a0d9f2fff5452ccbc2
Chris Underwood <cunderwoodmn@gmail.com>
2020-03-28 22:43:39 -0400

    set up the new account api to hash username and passwords before saving

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
-
 const AccountTable = require('../account/table');
+const { hash } = require('../account/helper');
 
 const router = new Router();
 
 router.post('/signup', async (req, res, next) => {
   try {
     const { username, password } = req.body;
+    const usernameHash = hash(username);
+    const passwordHash = hash(password);
 
-    const response = await AccountTable.storeAccount({ username, password });
+    const response = await AccountTable.storeAccount({ usernameHash, passwordHash });
     res.json({ message: 'success!' });
   } catch (err) {
     //res.json({ message: err.message });
