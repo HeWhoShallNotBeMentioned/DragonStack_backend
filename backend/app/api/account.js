fa12b0a81fe03c281fe5250b5df957e857aad95f
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-03 22:04:37 -0400

    start cookie process. Send cookie with new account generation.

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const AccountTable = require('../account/table');
 const { hash } = require('../account/helper');
+const Session = require('../account/session');
 
 const router = new Router();
 
@@ -16,6 +17,15 @@ router.post('/signup', async (req, res, next) => {
 
     if (!acctCheckResponse.account) {
       const acctCreateResponse = await AccountTable.storeAccount({ usernameHash, passwordHash });
+
+      const session = new Session({ username })
+      const sessionString = session.toString();
+      res.cookie('sessionString', sessionString, {
+        expire: Date.now() + 3600000,
+        httpOnly: true,
+        //Requires https connection for cookie access. Turned off as our dev env does not have https.
+        //secure: true
+      });
       res.json({ message: 'success!' });
     } else {
       const error = new Error("This username has already been taken.")
