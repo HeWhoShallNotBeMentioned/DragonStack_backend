832584c6772a2bf8f5838c852256d782299e260e
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-06 21:34:19 -0400

    created updateSessionId on the accountTableClass

--- backend/app/api/helper.js
+++ backend/app/api/helper.js
@@ -7,7 +7,7 @@ const setSession = ({ username, res }) => {
     const session = new Session({ username })
     const sessionString = session.toString();
 
-    AccountTable.updateSessionId({ sessionId: session.id, userNameHash: hash(username) });
+    AccountTable.updateSessionId({ sessionId: session.id, usernameHash: hash(username) });
 
     res.cookie('sessionString', sessionString, {
       expire: Date.now() + 3600000,
