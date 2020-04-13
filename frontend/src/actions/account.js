f48ba760f930d0aa5723c029abca6b35e288b369
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-13 19:02:19 -0400

    added thunk for retreiving an account

---
+++ frontend/src/actions/account.js
@@ -0,0 +1,33 @@
+import { ACCOUNT } from './types';
+import { BACKEND } from '../config';
+
+export const signup = ({ username, password }) => async dispatch => {
+  dispatch({ type: ACCOUNT.FETCH })
+  try {
+    let { data } = await fetch(`${BACKEND.ADDRESS}/account/signup`, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include'
+    }).json();
+    console.log("data ", data)
+    if (data.type === 'error') {
+      dispatch({
+        type: ACCOUNT.FETCH_ERROR,
+        message: data.message
+      })
+    } else {
+      return dispatch({
+        type: ACCOUNT.FETCH_SUCCESS,
+        ...data,
+      });
+    }
+
+  } catch (error) {
+    dispatch({
+      type: ACCOUNT.FETCH_ERROR,
+      message: error.message
+    })
+  }
+
+};
