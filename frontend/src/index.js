7fe18f031dedf32b898f234df716709fef996bb8
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-25 23:11:15 -0400

    add a redirect route to a sample componet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
 import createBrowserHistory from 'history/createBrowserHistory';
@@ -22,6 +22,14 @@ const store = createStore(
   )
 );
 
+class RedirectToAccountDragons extends Component {
+  render() {
+    return (
+      <Redirect to={{ pathname: '/account-dragons' }} />
+    )
+  }
+}
+
 store.dispatch(fetchAuthentiated())
   .then(() => {
     render(
@@ -30,6 +38,7 @@ store.dispatch(fetchAuthentiated())
           <Switch>
             <Route exact path='/' component={Root} />
             <Route path="/account-dragons" component={AccountDragons} />
+            <Route path='/redirect-to-account-dragons' component={RedirectToAccountDragons} />
           </Switch>
         </Router>
       </Provider>,
