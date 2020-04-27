c0600007e5abecd37c23f1ae71ce929bd75e5fb7
Chris Underwood <cunderwoodmn@gmail.com>
2020-04-26 21:35:28 -0400

    add text box to see the nickname. add buttons for save or edit. Disable the input unless the edit button has been selected. create local state for edit and nickname.

--- frontend/src/components/AccountDragonRow.js
+++ frontend/src/components/AccountDragonRow.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import DragonAvatar from './DragonAvatar';
+import { Button } from 'react-bootstrap';
 
 class AccountDragonRow extends Component {
+
+  state = {
+    nickname: this.props.dragon.nickname,
+    edit: false
+  }
+
+  updateNickname = event => {
+    this.setState({ nickname: event.target.value });
+  }
+
+  toggleEdit = () => {
+    this.setState({ edit: !this.state.edit })
+  }
+
+  get SaveButton() {
+    return <Button onClick={this.toggleEdit} variant="success" >Save</Button>
+  }
+
+  get EditButton() {
+    return <Button onClick={this.toggleEdit} variant="info"  >Edit</Button>
+  }
 
   render() {
     return (
       <div>
         <p>{this.props.dragon.nickname}</p>
+        <input
+          type="text"
+          value={this.state.nickname}
+          onChange={this.updateNickname}
+          disabled={!this.state.edit}
+        />
         <DragonAvatar dragon={this.props.dragon} />
+        {
+          this.state.edit
+            ? this.SaveButton
+            : this.EditButton
+        }
       </div>
     )
   }
