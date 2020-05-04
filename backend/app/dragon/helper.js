c8df94bcb19a425b4735f410728cf65a2a818877
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-03 20:33:44 -0400

    rewrote dragonWithTraits from .then block to async await

--- backend/app/dragon/helper.js
+++ backend/app/dragon/helper.js
@@ -2,30 +2,32 @@ const pool = require('../../databasePool');
 const DragonTable = require('./table');
 const Dragon = require('./index');
 
-const getDragonWithTraits = ({ dragonId }) => {
-  return Promise.all([
-    DragonTable.getDragon({ dragonId }),
-    new Promise((resolve, reject) => {
-      pool.query(`SELECT "traitType", "traitValue"
+const getDragonWithTraits = async ({ dragonId }) => {
+  try {
+    let dragonPromises = await Promise.all([
+      DragonTable.getDragon({ dragonId }),
+      new Promise((resolve, reject) => {
+        pool.query(`SELECT "traitType", "traitValue"
       FROM trait
       INNER JOIN dragonTrait ON trait.id = dragonTrait."traitId"
       WHERE dragonTrait."dragonId" = $1`,
-        [dragonId],
-        (error, response) => {
-          if (error) {
-            return reject(error);
-          };
+          [dragonId],
+          (error, response) => {
+            if (error) {
+              return reject(error);
+            };
 
-          resolve(response.rows);
-        }
-      )
-    })
-  ])
-    .then(([dragon, dragonTraits]) => {
-      return new Dragon({ ...dragon, dragonId, traits: dragonTraits })
-    })
-    .catch(error => console.error(error))
-};
+            resolve(response.rows);
+          }
+        )
+      })
+    ])
+
+    return new Dragon({ dragon: dragonPromises[0], dragonId, traits: dragonPromises[1] })
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 
 
