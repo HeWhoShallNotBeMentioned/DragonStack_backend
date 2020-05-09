f5624b57a5c771bc6420ab5c8c7ccb4df27a734b
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-09 16:24:45 -0400

    create first instance of Breeder class

---
+++ backend/app/dragon/breeder.js
@@ -0,0 +1,33 @@
+const Dragon = require('./index');
+
+class Breeder {
+  static breedDragon({ matron, patron }) {
+    const matronTraits = matron.traits;
+    const patronTraits = patron.traits;
+
+    const babyTraits = [];
+
+    matronTraits.forEach(({ traitType, traitValue }) => {
+      const matronTrait = traitValue;
+
+      // const patronTrait = patronTraits.find(
+      //   trait => trait.traitType === traitType
+      // );
+
+      const patronTrait = patronTraits[traitType];
+
+      babyTraits.push({
+        traitType,
+        traitValue: Breeder.pickTrait({
+          matronTrait, patronTrait
+        })
+      });
+    })
+    return new Dragon({ nickname: 'Unnamed baby', traits: babyTraits })
+  }
+
+
+
+}
+
+module.exports = Breeder;
