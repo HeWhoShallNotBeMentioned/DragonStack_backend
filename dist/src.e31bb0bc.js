3cfa3284efe17675c5edee0ce28183df12010011
Chris Underwood <cunderwoodmn@gmail.com>
2020-05-12 13:46:33 -0400

    add plugins from babelrc file to node modules via npm i

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -53537,7 +53537,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55444" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57133" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
