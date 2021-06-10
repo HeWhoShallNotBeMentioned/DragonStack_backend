6966008b9231b4cf39245726240b3f9b67f882dc
Chris Underwood <cunderwoodmn@gmail.com>
2021-06-10 16:21:22 -0700

    add ssl rejectUnauthorized: false to db config

--- backend/secrets/databaseConfiguration.js
+++ backend/secrets/databaseConfiguration.js
@@ -1,1 +1,10 @@
-module.exports = { user: 'node_user', host: 'localhost', database: 'dragonstackdb', password: 'node_password', port: 5432 }
+module.exports = {
+  user: 'node_user',
+  host: 'localhost',
+  database: 'dragonstackdb',
+  password: 'node_password',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+  },
+};
