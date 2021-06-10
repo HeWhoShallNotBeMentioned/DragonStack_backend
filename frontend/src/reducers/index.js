42f8b9b639f7d83d188297098617c563642c6f13
Chris Underwood <cunderwoodmn@gmail.com>
2021-06-10 16:34:58 -0700

    removed frontend files from this project.

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import generation from './generation';
-import dragon from './dragon';
-import account from './account';
-import accountDragons from './accountDragons';
-import accountInfo from './accountInfo';
-import publicDragons from './publicDragons';
-
-export default combineReducers({ generation, dragon, account, accountDragons, accountInfo, publicDragons });
